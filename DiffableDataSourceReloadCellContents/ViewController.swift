e1deaba826a6bf643eb4531289149d0d8b49f6ff
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-07-23 13:51:59 +0900

    🎨: FruitとItemを分離することで、管理しやすくする
    - itemはあくまでIndexを保持するだけにする

--- DiffableDataSourceReloadCellContents/ViewController.swift
+++ DiffableDataSourceReloadCellContents/ViewController.swift
@@ -32,51 +32,48 @@ class ViewController: UIViewController {
         case `default`
     }
 
-    private struct FruitItem: Hashable {
-        let id: UUID
-        let fruitInfo: FruitInfo
+    private struct Item: Hashable {
+        let fruitId: UUID
+    }
 
-        init(id: UUID, name: String, isFavorite: Bool) {
-            self.id = id
-            self.fruitInfo = .init(name: name, isFavorite: isFavorite)
+    private struct Fruit {
+        var name: String
+        var isFavorite: Bool
+
+        init(name: String, isFavorite: Bool) {
+            self.name = name
+            self.isFavorite = isFavorite
         }
 
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(id)
-        }
-
-        public static func ==(lhs: FruitItem, rhs: FruitItem) -> Bool {
-            lhs.id == rhs.id
-                && lhs.fruitInfo === rhs.fruitInfo
-        }
-
-        class FruitInfo {
-            var name: String
-            var isFavorite: Bool
-
-            init(name: String, isFavorite: Bool) {
-                self.name = name
-                self.isFavorite = isFavorite
-            }
+        mutating func toggleFavorite() {
+            isFavorite = !isFavorite
         }
     }
 
-    private var fruits: [FruitItem] = [.init(id: UUID(), name: "Grape Fruits", isFavorite: false),
-                                       .init(id: UUID(), name: "Orange", isFavorite: false),
-                                       .init(id: UUID(), name: "Cabos", isFavorite: false),
-                                       .init(id: UUID(), name: "Citron", isFavorite: true),
-                                       .init(id: UUID(), name: "Lime", isFavorite: false),
-                                       .init(id: UUID(), name: "Bergamot", isFavorite: false),
-                                       .init(id: UUID(), name: "Iyokan Orange", isFavorite: false),
-                                       .init(id: UUID(), name: "Dekopon", isFavorite: false)] {
+    private var fruits: [UUID: Fruit] = [.init(): .init(name: "Grape Fruits", isFavorite: false),
+                                         .init(): .init(name: "Orange", isFavorite: false),
+                                         .init(): .init(name: "Cabos", isFavorite: false),
+                                         .init(): .init(name: "Citron", isFavorite: true),
+                                         .init(): .init(name: "Lime", isFavorite: false),
+                                         .init(): .init(name: "Bergamot", isFavorite: false),
+                                         .init(): .init(name: "Iyokan Orange", isFavorite: false),
+                                         .init(): .init(name: "Dekopon", isFavorite: false)] {
         didSet {
             updateDataSource()
         }
     }
 
-    private lazy var collectionViewDataSource: UICollectionViewDiffableDataSource<Section, FruitItem> = .init(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, item: FruitItem) -> UICollectionViewCell? in
+    private var items: [Item] {
+        fruits.sorted(by: { $0.value.name < $1.value.name })
+            .map { Item(fruitId: $0.key) }
+    }
+
+    private lazy var collectionViewDataSource: UICollectionViewDiffableDataSource<Section, Item> = .init(collectionView: collectionView) { [weak self] (collectionView: UICollectionView, indexPath: IndexPath, item: Item) -> UICollectionViewCell? in
+        guard let _self = self,
+              let fruit = _self.fruits[item.fruitId] else { return nil }
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FruitCell", for: indexPath) as! FruitCell
-        cell.viewModel = .init(name: item.fruitInfo.name, isFavorite: item.fruitInfo.isFavorite)
+        cell.viewModel = .init(name: fruit.name, isFavorite: fruit.isFavorite)
         return cell
     }
 
@@ -94,26 +91,28 @@ class ViewController: UIViewController {
 
     /// CollectionViewのデータソースを更新する
     private func updateDataSource() {
-        var snapshot = NSDiffableDataSourceSnapshot<Section, FruitItem>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         snapshot.appendSections([.default])
-        snapshot.appendItems(fruits)
+        snapshot.appendItems(items)
 
         collectionViewDataSource.apply(snapshot, animatingDifferences: true)
     }
 
     // セルのインスタンスは更新せず、中身だけ更新する
-    private func reloadContentsOfItem(item: FruitItem) {
+    private func reloadContentsOfItem(item: Item) {
         guard let indexPath = collectionViewDataSource.indexPath(for: item),
-              let cell = collectionView.cellForItem(at: indexPath) as? FruitCell else { return }
-        cell.viewModel = .init(name: item.fruitInfo.name,
-                               isFavorite: item.fruitInfo.isFavorite)
+              let cell = collectionView.cellForItem(at: indexPath) as? FruitCell,
+              let fruit = fruits[item.fruitId] else { return }
+        cell.viewModel = .init(name: fruit.name,
+                               isFavorite: fruit.isFavorite)
     }
 }
 
 extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let fruitTapped = fruits[indexPath.row]
-        fruits.filter({ $0 == fruitTapped }).forEach { $0.fruitInfo.isFavorite = !$0.fruitInfo.isFavorite }
-        reloadContentsOfItem(item: fruitTapped)
+        if let itemTapped = collectionViewDataSource.itemIdentifier(for: indexPath) {
+            fruits[itemTapped.fruitId]?.toggleFavorite()
+            reloadContentsOfItem(item: itemTapped)
+        }
     }
 }
